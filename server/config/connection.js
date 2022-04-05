115f6e8a3f57c7fa0b106b929489bbeb85c5745a
coleleg <coleleg@gmail.com>
2022-04-04 20:19:25 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://coleleg:zN4u9KGuegYM3rwo@cluster0.ites=true&w=msmi5o.mongodb.net/myFirstDatabase?retryWrajority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
