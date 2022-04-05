295945f6d015eb9012145849a4137144110e383c
coleleg <coleleg@gmail.com>
2022-04-04 20:26:05 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://coleleg:zN4u9KGuegYM3rwo@cluster0.ites=true&w=msmi5o.mongodb.net/book-search-engine?retryWrajority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
